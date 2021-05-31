c9f26ecc75d35c79c3bb0cac8fca49fa5c34e1e4
Eric Zepeda <zepedaeric@gmail.com>
2021-05-31 14:55:41 -0700

    Define Error messages

--- src/main/java/com/example/menu/item/ItemController.java
+++ src/main/java/com/example/menu/item/ItemController.java
@@ -6,12 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/menu/items")
@@ -36,7 +43,7 @@ public class ItemController {
     }
 
     @PostMapping
-    public ResponseEntity<Item> create(@RequestBody Item item) {
+    public ResponseEntity<Item> create(@Valid @RequestBody Item item) {
         Item created = service.create(item);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
@@ -48,7 +55,7 @@ public class ItemController {
     @PutMapping("/{id}")
     public ResponseEntity<Item> update(
             @PathVariable("id") Long id,
-            @RequestBody Item updatedItem) {
+            @Valid @RequestBody Item updatedItem) {
 
         Optional<Item> updated = service.update(id, updatedItem);
 
@@ -68,5 +75,17 @@ public class ItemController {
     public ResponseEntity<Item> delete(@PathVariable("id") Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+        Map<String, String> map = new HashMap<>(errors.size());
+        errors.forEach((error) -> {
+            String key = ((FieldError) error).getField();
+            String val = error.getDefaultMessage();
+            map.put(key, val);
+        });
+        return ResponseEntity.badRequest().body(map);
     }
 }
